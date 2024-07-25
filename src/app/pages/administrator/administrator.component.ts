21834dff599668c0c89e2baded3176e5a0bb228b
Qolby Latifah <qolby.latifah@wgs-id.com>
2024-07-25 14:15:40 +0700

    💄 Create Edit Administrator

--- src/app/pages/administrator/administrator.component.ts
+++ src/app/pages/administrator/administrator.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./administrator.component.scss']
 })
 export class AdministratorComponent {
+  isModalOpen = false; // Track modal open state
+  title = '';
 
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+
+  openModal(type: string, id: any) {
+    this.title = type;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
