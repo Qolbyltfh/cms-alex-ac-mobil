fd42af5b3acdb975fc1c5c687b3840d51df29b8d
Qolby Latifah <qolby.latifah@wgs-id.com>
2024-07-25 11:35:43 +0700

    💄 Logout and adjust layout

--- src/app/modules/main/header/header.component.ts
+++ src/app/modules/main/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  isDropdownOpen = false;
 
+  constructor(
+    private router: Router,
+  ) {
+    
+  }
+
+  ngOnInit() {
+
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  logout(){
+    // redirect to login page
+    this.router.navigate(['/login']);
+
+  }
 }
