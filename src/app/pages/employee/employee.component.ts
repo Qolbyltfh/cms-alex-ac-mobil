57400dc13c49968e975e50795cf33e8b4a78fa32
Qolby Latifah <qolby.latifah@wgs-id.com>
2024-07-25 14:18:44 +0700

    💄 Create Edit Employee

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./employee.component.scss']
 })
 export class EmployeeComponent {
+  isModalOpen = false; // Track modal open state
+  title = '';
 
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen;
+  }
+
+  openModal(type: string, id: any) {
+    this.title = type;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
 }
